c3e262c664018691bfbc9e0ec090cfdd43a476da
rodneitsumoto <rodneitsumoto@N1591614521-0>
2022-03-28 17:26:21 -0300

    Update_de_Runin_com_Switch

---
+++ RuninNotebookAPI/Controllers/RuninLayOutController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using RuninNotebookAPI.Models;
+using RuninNotebookAPI.DB;
+using System.Web.Http;
+
+namespace RuninNotebookAPI.Controllers
+{
+    public class RuninLayOutController : ApiController
+    {
+
+        public object MSG { get; set; }
+
+        [HttpGet]
+        public IHttpActionResult OUT_GET(string idRunin)
+        {
+            if (idRunin is null)
+            {
+                NotFound();
+            }
+
+            string[] Columns = idRunin.Split(',');
+
+            Runin_LayOut rl = new Runin_LayOut();
+
+            try
+            {
+                if (Convert.ToInt32(Columns[0]) > 0 &&  Columns[1].Contains("10.8.35") && Convert.ToInt32(Columns[2]) > 0 && (Columns[3].Contains("192.168") || Columns[3].Contains("172.168")))
+                {
+                    ConexaoDB.CRUD_tabela($@"update Runin_LayOut  set  IP_NB='{Columns[3].ToString()}' where idRunin_Layout={Columns[0]}");
+
+                    return Json("set result=0");
+                }
+                else
+                {
+                    return Json("Falta paramenters ");
+                }
+            }
+            catch (Exception)
+            {
+
+                return Json("Problema ao gravar Runin_Layout"); ;
+            }
+            
+
+
+        }
+
+    }
+}
---
+++ RuninNotebookAPI/Models/Runin_LayOut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RuninNotebookAPI.Models
+{
+    public class Runin_LayOut
+    {
+
+        public int idRunin_Layout { get; set; }
+        public string Descricao { get; set; }
+        public string Lado { get; set; }
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public int Position { get; set; }
+        public int Status_Code { get; set; }
+        public int idproduct_movement { get; set; }
+        public string Chamado_IT { get; set; }
+        public string Data_Parada { get; set; }
+        public string IP_Address { get; set; }
+        public string Switch_Port { get; set; }
+        public string Note { get; set; }
+        public string IP_NB { get; set; }
+    }
+}
